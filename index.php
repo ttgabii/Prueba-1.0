95b12ba8e939205b778110d7316d06e57f67bc66
perico <perico@gmail.com>
2024-10-25 10:44:21 +0200

    Revert "One more"
    This reverts commit 92a0b6f1438390457f7b78ae279ed630175f3f36.

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 echo 'Al gener, tanca la porta i encén el braser';
 echo 'Aigua de gener tot l'any va bé';
 echo 'Les gelades de gener fan florir l'ametller';
+echo 'pel febrer, carnestoltes el primer';
+echo 'Pel febrer flor a l'ametler';
