92a0b6f1438390457f7b78ae279ed630175f3f36
perico <perico@gmail.com>
2024-10-25 10:43:52 +0200

    One more

--- febrer.php
+++ febrer.php
@@ -1,3 +1,4 @@
 echo 'Més tard o més matiner carnaval dins el febrer';
 echo 'Pel febrer flor a l'ametler';
 echo 'pel febrer, carnestoltes el primer';
+echo 'Has de llaurar al Febrer el que al marc vulgues sembrar';
